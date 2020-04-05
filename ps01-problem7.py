d0b9a1062f13ffd81ac79045ccdce7ff8e9872b1
nezihoglu <63059564+nezihoglu@users.noreply.github.com>
2020-04-05 11:06:02 +0600

    Add files via upload

---
+++ ps01-problem7.py
@@ -0,0 +1,10 @@
+print ('''A well-formed Java program has
+
+a main method with { and }
+braces.
+
+A System.out.println statement
+has ( and ) and usually a
+String that starts and ends
+with a " character.
+(But we type \" instead!)''')
