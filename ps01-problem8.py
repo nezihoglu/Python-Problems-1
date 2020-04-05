14526547f48d87d29f704726b0e789c33af1ba84
nezihoglu <63059564+nezihoglu@users.noreply.github.com>
2020-04-05 11:06:26 +0600

    Add files via upload

---
+++ ps01-problem8.py
@@ -0,0 +1,5 @@
+print ('''What is the difference between
+a ' and a "?  Or between a " and a \"?
+
+One is what we see when we're typing our program.
+The other is what appears on the "console."''')
