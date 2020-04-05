8e4a9951d6973ce1fbd0561b2dc05d13982bf63e
nezihoglu <63059564+nezihoglu@users.noreply.github.com>
2020-04-05 11:07:15 +0600

    Add files via upload

---
+++ ps01-problem10.py
@@ -0,0 +1,1 @@
+print ('A "quoted" String is\n "much" better if you learn\n the rules of "escape sequences."\n Also, "" represents an empty String.\n Don"t forget: use \" instead of " !\n '' is not the same as')
